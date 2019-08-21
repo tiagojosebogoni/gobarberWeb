0981220a42545834eb85d99d67a2b79d25392538
tiago <tiagojosebogoni@gmail.com>
2019-08-20 22:49:51 -0300

    adicionado saga e redux

--- src/pages/SigIn/index.js
+++ src/pages/SigIn/index.js
@@ -1,16 +1,32 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { Form, Input } from '@rocketseat/unform';
+import * as Yup from 'yup';
+import { signInRequest } from '../../store/modules/auth/actions';
 
 import logo from '../../assets/logo.svg';
 // import { Container } from './styles';
 
+const schema = Yup.object().shape({
+  email: Yup.string()
+    .email('Insira seu e-mail')
+    .required('E-mail é obrigatório'),
+  password: Yup.string().required('Senha é obrigaória'),
+});
+
 export default function SigIn() {
+  const dispatch = useDispatch();
+
+  function handleSubmit({email, password}) {
+    dispatch(signInRequest(email, password)
+  }
+
   return (
     <>
       <img src={logo} alt="GoBarber" />
 
-      <Form>
+      <Form schema={schema} onSubmit={handleSubmit}>
         <Input name="email" type="text" placeholder="Seu Email" />
         <Input name="password" type="password" placeholder="Sua senha" />
 
--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,7 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { Form, Input } from '@rocketseat/unform';
+import * as Yup from 'yup';
 import logo from '../../assets/logo.svg';
+
+const schema = Yup.object().shape({
+  name: Yup.string().required('Nome é obrigatório'),
+  email: Yup.string()
+    .email('Insira seu e-mail')
+    .required('E-mail é obrigatório'),
+  password: Yup.string()
+    .min(6, 'No mínimo 6 caracteres')
+    .required('Senha é obrigaória'),
+});
 
 export default function SigUp() {
   function handleSubmit() {}
@@ -10,7 +21,7 @@ export default function SigUp() {
     <>
       <img src={logo} alt="GoBarber" />
 
-      <Form onSubmit={handleSubmit}>
+      <Form schema={schema} onSubmit={handleSubmit}>
         <Input name="name" placeholder="Nome Completo" />
         <Input name="email" type="text" placeholder="Seu Email" />
         <Input name="password" type="password" placeholder="Sua senha" />
