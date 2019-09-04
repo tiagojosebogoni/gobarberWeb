a462c9973c1914789a74cde83a07ddc63bb521c8
tiago <tiagojosebogoni@gmail.com>
2019-09-04 16:37:38 -0300

    Adicionado funcionalidade de criar nova conta

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { Form, Input } from '@rocketseat/unform';
 import * as Yup from 'yup';
+
 import logo from '../../assets/logo.svg';
+import { signUpRequest } from '../../store/modules/auth/actions';
 
 const schema = Yup.object().shape({
   name: Yup.string().required('Nome é obrigatório'),
@@ -15,7 +18,11 @@ const schema = Yup.object().shape({
 });
 
 export default function SigUp() {
-  function handleSubmit() {}
+  const dispatch = useDispatch();
+
+  function handleSubmit({ name, email, password }) {
+    dispatch(signUpRequest(name, email, password));
+  }
 
   return (
     <>
