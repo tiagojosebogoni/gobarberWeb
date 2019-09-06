8829cc0f8a34755cb7de10336fd629cd3736bc04
tiago <tiagojosebogoni@gmail.com>
2019-09-05 21:59:52 -0300

    Alterado para fazer a leitura dos dados do Redux no Header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+
 import { Link } from 'react-router-dom';
 import Notifications from '../Notifications';
 import logo from '../../assets/logo-purple.svg';
@@ -6,6 +8,8 @@ import logo from '../../assets/logo-purple.svg';
 import { Container, Content, Profile } from './styles';
 
 export default function Header() {
+  const profile = useSelector(state => state.user.profile);
+
   return (
     <Container>
       <Content>
@@ -18,11 +22,14 @@ export default function Header() {
           <Notifications />
           <Profile>
             <div>
-              <strong>Tiago Bogoni</strong>
+              <strong>{profile.name}</strong>
               <Link to="/profile">Meu Perfil</Link>
             </div>
             <img
-              src="https://api.adorable.io/avatars/50/abott@adorable.png"
+              src={
+                profile.avatar.url ||
+                'https://api.adorable.io/avatars/50/abott@adorable.png'
+              }
               alt="Tiago "
             />
           </Profile>
