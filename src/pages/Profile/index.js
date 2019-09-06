3c21724a0793dd563cedb1e102dd54b82ee4ea89
tiago <tiagojosebogoni@gmail.com>
2019-09-05 21:55:02 -0300

    Adicionado para atualizar foto de perfil

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,16 +1,23 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { Form, Input } from '@rocketseat/unform';
+import { updateProfileRequest } from '../../store/modules/user/actions';
+
+import AvatarInput from './AvatarInput';
 import { Container } from './styles';
 
 export default function Profile() {
+  const dispatch = useDispatch();
   const profile = useSelector(state => state.user.profile);
 
-  function handleSubmit(data) {}
+  function handleSubmit(data) {
+    dispatch(updateProfileRequest(data));
+  }
 
   return (
     <Container>
       <Form initialData={profile} onSubmit={handleSubmit}>
+        <AvatarInput name="avatar_id" />
         <Input name="name" placeholder="Nome completo" />
         <Input name="email" type="email" placeholder="Seu Email" />
 
