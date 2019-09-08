b0ca815a28b62ce3532053695ea191babf955781
tiago <tiagojosebogoni@gmail.com>
2019-09-08 20:44:03 -0300

    Adicionado funcionalidade para poder navegar entre datas

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,18 +1,34 @@
-import React from 'react';
+import React, { useState, useMemo } from 'react';
+import { format, subDays, addDays } from 'date-fns';
+import pt from 'date-fns/locale/pt';
 import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
 // import api from '../../services/api';
 
 import { Container, Time } from './styles';
 
 export default function Dashboard() {
+  const [date, setDate] = useState(new Date());
+  const dateFormatted = useMemo(
+    () => format(date, "d 'de' MMMM", { locale: pt }),
+    [date]
+  );
+
+  function handlePrevDay() {
+    setDate(subDays(date, 1));
+  }
+
+  function handleAddDay() {
+    setDate(addDays(date, 1));
+  }
+
   return (
     <Container>
       <header>
-        <button>
+        <button type="button" onClick={handlePrevDay}>
           <MdChevronLeft size={36} color="#FFF" />
         </button>
-        <strong>31 de maio</strong>
-        <button>
+        <strong>{dateFormatted}</strong>
+        <button type="button" onClick={handleAddDay}>
           <MdChevronRight size={36} color="#FFF" />
         </button>
       </header>
