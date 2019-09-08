c6539f16e0dc384e710b4b7bae49b6670fe70628
tiago <tiagojosebogoni@gmail.com>
2019-09-08 20:36:31 -0300

    Adicionado stilos na pagina Dasboard

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,9 +1,39 @@
 import React from 'react';
-import api from '../../services/api';
+import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
+// import api from '../../services/api';
 
-// import { Container } from './styles';
+import { Container, Time } from './styles';
 
 export default function Dashboard() {
-  api.get('appointment');
-  return <div>Dashboard</div>;
+  return (
+    <Container>
+      <header>
+        <button>
+          <MdChevronLeft size={36} color="#FFF" />
+        </button>
+        <strong>31 de maio</strong>
+        <button>
+          <MdChevronRight size={36} color="#FFF" />
+        </button>
+      </header>
+      <ul>
+        <Time past>
+          <strong>08:00</strong>
+          <span>Tiago Bogoni</span>
+        </Time>
+        <Time available>
+          <strong>09:00</strong>
+          <span>Em Aberto</span>
+        </Time>
+        <Time available>
+          <strong>10:00</strong>
+          <span>Em Aberto</span>
+        </Time>
+        <Time available>
+          <strong>11:00</strong>
+          <span>Em Aberto</span>
+        </Time>
+      </ul>
+    </Container>
+  );
 }
